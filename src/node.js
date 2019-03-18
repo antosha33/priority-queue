78b9c6d9ec0b8ffe0273f8196b9451e36fd045f4
Anton Yegorov <an_tosha33@mail.ru>
2019-03-18 15:28:05 +0300

    add node appendChild method

--- src/node.js
+++ src/node.js
@@ -1,27 +1,32 @@
 class Node {
-	constructor(data, priority) {
+  constructor(data, priority) {
     this.data = data;
     this.priority = priority;
     this.parent = null;
     this.left = null;
     this.right = null;
-	}
+  }
 
-	appendChild(node) {
+  appendChild(node) {
+    if (this.left == null) {
+      this.left = node;
+      this.right = null;
+    }else if(this.right == null){
+      this.right = node;
+    }
+  }
 
-	}
+  removeChild(node) {
 
-	removeChild(node) {
+  }
 
-	}
+  remove() {
 
-	remove() {
+  }
 
-	}
+  swapWithParent() {
 
-	swapWithParent() {
-		
-	}
+  }
 }
 
 module.exports = Node;
