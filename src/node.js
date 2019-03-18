3ecf8c45c8a079a017431337ede1794bca8efc39
Anton Yegorov <an_tosha33@mail.ru>
2019-03-18 16:59:18 +0300

    feat: add node appendChild removeChild remove

--- src/node.js
+++ src/node.js
@@ -11,17 +11,28 @@ class Node {
     if (this.left == null) {
       this.left = node;
       this.right = null;
+      node.parent = this;
     }else if(this.right == null){
       this.right = node;
+      node.parent = this;
     }
   }
 
   removeChild(node) {
-
+    if(this.left == node){
+      this.left = null;
+      node.parent = null;
+    }else if(this.right == node){
+      this.right = null;
+      node.parent = null;
+    }else{
+      throw new Error();
+    }
   }
 
   remove() {
-
+    if(this.parent == null) return
+    this.parent.removeChild(this);
   }
 
   swapWithParent() {
