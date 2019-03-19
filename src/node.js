4df1db43e4cfe5c214d62c7faf554b443bd93575
Anton Yegorov <an_tosha33@mail.ru>
2019-03-19 09:54:58 +0300

    feat: funish node

--- src/node.js
+++ src/node.js
@@ -39,11 +39,25 @@ class Node {
     var buffer;
     if(this.parent == null){
       return;
-    }else if(this.parent != null && this.parent.right == null){
-      buffer = this.parent.parent;
-      this.parent.parent = this;
-      this.parent = buffer;
-    }else if(this.parent!= null && this.right==null && this.left != null){
+
+    }
+    else if(this.parent != null && this.parent.parent != null && this.parent.parent.right != null){
+      var branch;
+      for(var key in this.parent.parent){
+        if(this.parent.parent[key] == this.parent) {
+          console.log(key);
+          branch = key;
+          break;
+        }
+      }
+      buffer = this.parent;
+      this.parent.parent[branch] = this;
+      this.parent = this.parent.parent;
+      this.left = buffer;
+      buffer.parent = this;
+      buffer.left = null;
+    }
+    else if(this.parent!= null && this.right==null && this.left != null && this.parent.right.left == null){
       buffer = this.left;
       this.left = this.parent;
       this.left.left = buffer;
@@ -52,11 +66,20 @@ class Node {
       this.left.right = null;
       this.left.parent = this;
       this.parent = null
-    }else{
+    }else if (this.parent!= null && this.right == null && this.left == null && this.parent.left !=null && this.parent.right != null){
       buffer = this;
       this.parent.left.parent = buffer;
       this.parent.parent = buffer;
       this.parent = null;
+    }else{
+        // buffer = this.parent;
+        // this.parent.parent.left = this;
+        // this.parent.left = buffer;
+        // this.parent = null;
+
+        buffer = this.parent.parent;
+        this.parent.parent = this;
+        this.parent = buffer;
     }
   }
 }
